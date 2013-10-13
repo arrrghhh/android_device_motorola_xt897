744335c177b9e1bb105b9e236ee949a17cc9c2a0
arrrghhh <arrrghhh@xdandroid.com>
2013-10-13 13:58:29 -0600

    Change cm.mk to cos.mk

--- cos.mk
+++ cos.mk
@@ -1,11 +1,11 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cos/config/gsm.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common ChaOS stuff.
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cos/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
@@ -13,7 +13,7 @@ TARGET_SCREEN_HEIGHT := 960
 
 # Release name
 PRODUCT_RELEASE_NAME := xt897
-PRODUCT_NAME := cm_xt897
+PRODUCT_NAME := cos_xt897
 
 $(call inherit-product, device/motorola/xt897/full_xt897.mk)
 

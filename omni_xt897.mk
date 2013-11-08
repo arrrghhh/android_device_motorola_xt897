f9bdaa8d5d88553d40494c5a52acd1e498a596f6
arrrghhh <arrrghhh@xdandroid.com>
2013-11-07 17:26:15 -0700

    Missing in last commit?

---
+++ omni_xt897.mk
@@ -0,0 +1,24 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 540
+TARGET_SCREEN_HEIGHT := 960
+
+# Release name
+PRODUCT_RELEASE_NAME := xt897
+PRODUCT_NAME := custom_xt897
+
+$(call inherit-product, device/motorola/xt897/full_xt897.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND=motorola PRODUCT_NAME=xt897 BUILD_PRODUCT=xt897 BUILD_FINGERPRINT=motorola/XT897_us_spr/asanti_c:4.1.2/9.8.2Q-122_XT897_FFW-5/6:user/release-keys
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := xt897
+PRODUCT_NAME := omni_xt897
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := XT897
+PRODUCT_MANUFACTURER := motorola
